415f948d0d2293b7bb54d33aed0eaf9994ff7148
JoeLee <lizhou1021@qq.com>
2024-04-21 15:27:11 +0800

    first commit

---
+++ README.run.md
@@ -0,0 +1,3 @@
+```
+yolo segment predict model=runs/segment/train/weights/best.pt source='test3.jpg' save_txt=True
+```
---
+++ client/src/App.tsx
@@ -0,0 +1,104 @@
+import { createSignal, onCleanup, onMount } from "solid-js";
+import "./App.css";
+import * as BABYLON from "@babylonjs/core";
+
+function App() {
+  let canvasRef: HTMLCanvasElement | undefined;
+
+  let engine: BABYLON.Engine | undefined;
+
+  const click = async () => {
+    const text = await fetch(
+      "http://127.0.0.1:5000/runs/segment/predict7/labels/test3.txt"
+    ).then((res) => res.text());
+    const strArr = text.split("\r\n");
+
+    for (const str of strArr) {
+      const arr = str.split(" ");
+      const t = arr.shift();
+      // if (t !== "9") continue;
+      const pts: BABYLON.Vector3[] = [];
+      for (let i = 0; i < arr.length; i += 2) {
+        const sx = arr[i];
+        const sy = arr[i + 1];
+        const x = parseFloat(sx) * 100;
+        const y = parseFloat(sy) * 100;
+        const pt = new BABYLON.Vector3(x, 0, y);
+
+        pts.push(pt);
+      }
+
+      pts.push(pts[0].clone());
+
+      const line = BABYLON.MeshBuilder.CreateLines("line", { points: pts });
+
+      let extruded = BABYLON.MeshBuilder.ExtrudePolygon("ext", {
+        shape: pts, //vec3 array with z = 0,
+        depth: 20,
+        updatable: true,
+        sideOrientation: BABYLON.Mesh.DOUBLESIDE
+      });
+      extruded.position.y=20
+    }
+  };
+
+  onMount(() => {
+    console.log(canvasRef);
+    if (canvasRef) {
+      canvasRef.width = window.innerWidth;
+      canvasRef.height = window.innerHeight;
+      engine = new BABYLON.Engine(canvasRef);
+      // Creates a basic Babylon Scene object
+      const scene = new BABYLON.Scene(engine);
+      // Creates and positions a free camera
+      const camera = new BABYLON.ArcRotateCamera(
+        "camera1",
+        0,
+        0,
+        100,
+        new BABYLON.Vector3(0),
+        scene
+      );
+      // Targets the camera to scene origin
+      camera.setTarget(BABYLON.Vector3.Zero());
+      // Attaches the camera to the canvas
+      camera.attachControl(canvasRef, true);
+      // Creates a light, aiming 0,1,0
+      const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+      );
+      // Dim the light a small amount 0 - 1
+      light.intensity = 0.7;
+
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
+    }
+  });
+
+  onCleanup(() => {
+    if (engine) {
+      engine.dispose();
+    }
+  });
+
+  return (
+    <div>
+      <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />
+      <button
+        onClick={click}
+        style={{
+          position: "fixed",
+          right: "40px",
+          top: 0,
+        }}
+      >
+        调试
+      </button>
+    </div>
+  );
+}
+
+export default App;
